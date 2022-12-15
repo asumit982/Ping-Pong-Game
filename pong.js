8dce42c7813d30394148adff99b9c3906945e3bf
sumittricon <sumit.anshul@triconinfotech.com>
2022-12-15 12:54:23 +0530

    Added text function and created a text

--- pong.js
+++ pong.js
@@ -14,9 +14,18 @@ drawRect(0,0,cvs.width,cvs.height,"BLACK");
 function drawCircle(x,y,r,color){
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.Pi*2,false);
+    ctx.arc(x,y,r,0,Math.PI*2,false);
     ctx.closePath();
     ctx.fill();
 }
 
-drawCircle(100,100,50,"WHITE");
+drawCircle(100, 100, 50,"WHITE");
+
+//draw Text
+function drawText(text,x,y,color){
+    ctx.fillStyle = color;
+    ctx.font = "45px fanatsy";
+    ctx.fillText(text,x,y);
+}
+
+drawText("something",300,200,"WHITE");
